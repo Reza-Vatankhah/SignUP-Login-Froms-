186188f3657dc7e24139430fbacd9917e0764dec
Reza Vatankhah <vatanxahreza21@gmail.com>
2022-06-02 16:19:56 +0430

    SignUp and Login forms with Validation

---
+++ src/components/validate.jsx
@@ -0,0 +1,47 @@
+const validate = (data, type) => {
+    const errors = {}
+
+    if (!data.email) {
+        errors.email = "Email address is required"  
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+        errors.email = "Email address is invalid"
+    } else {
+        delete errors.email
+    }
+
+    if (!data.password) {
+        errors.password = "Password is required"
+    } else if (data.password.length < 6 ) {
+        errors.password = "Password must be at least 6 characters"
+    } else {
+        delete errors.password
+    }
+     
+
+    if (type === "signup") {
+        if (!data.name.trim()) {
+            errors.name = "Username is required"
+        } else {
+            delete errors.name
+        }
+
+        if (!data.confirmPassword) {
+            errors.confirmPassword = "Confirm Password"
+        } else if (data.confirmPassword !== data.password) {
+            errors.confirmPassword = "Password does not match"
+        } else {
+            delete errors.confirmPassword
+        }
+
+        if (data.isAccepted) {
+            delete errors.isAccepted
+        } else {
+            errors.isAccepted = "Accept our regulations"
+        }
+    }
+
+    return errors;
+
+};
+
+export default validate;
